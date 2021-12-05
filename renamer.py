2e44e409e34556476339a954e0669ec8963d3cc0
TiredBob <supercrackmunkey@gmail.com>
2021-12-04 19:12:13 -0700

    Moved some code to the proper function. Added variable for readability.

--- renamer.py
+++ renamer.py
@@ -77,14 +77,8 @@ def list_filetypes(fileList): #Creates and returns a list.
 	#WIP Need to sort alphabetically.
 	for i in range (len(fileList)):
 		tempType = checkFileType(str(fileList[i]))
-		if ((tempType not in fileTypes) and (tempType != None)):
+		if ((tempType not in fileTypes) and (tempType is not None)):
 			fileTypes.append(tempType)
-	return fileTypes
-
-def list_files():
-	fileList = updateList() #Updates each time the button is pressed
-	# print (fileList)
-	fileTypes = list_filetypes(fileList)
 	for i in range(10):
 		if (i < 5):
 			if (i < len(fileTypes)):
@@ -93,13 +87,20 @@ def list_files():
 			elif (i >= len(fileTypes)):
 				typeS[i].visible = False
 		if (i >= 5 and i < 10):
+			offset = i-5
 			if (i < len(fileTypes)):
-				typeS2[i-5].text = fileTypes[i]
-				print(fileTypes[i])
-				typeS2[i-5].visible = True
+				typeS2[offset].text = fileTypes[i]
+				# print(fileTypes[i] + str(i))
+				typeS2[offset].visible = True
 			elif (i >= len(fileTypes)):
 				# print("I =" + str(i-5))
-				typeS2[i-5].visible = False
+				typeS2[offset].visible = False
+	return fileTypes
+
+def list_files():
+	fileList = updateList() #Updates each time the button is pressed
+	# print (fileList)
+	fileTypes = list_filetypes(fileList)
 	testList = createTextBoxes(fileList, len(fileList))
 	if (len(testList) > len(fileList)): #Checks to see if any files were removed from CWD.
 		removeTextBoxes(testList, (len(testList) - len(fileList)))
@@ -125,10 +126,11 @@ menubar = MenuBar(app, toplevel=["File", "Edit"], options=
 			["Option Edit", menuFakeFunc], ["Edit 2", menuFakeFunc]
 		]
 	])
+
 buttonBox = Box(app, grid=[0,1], align="left", layout="grid")
 button = PushButton(app, list_files, text="Update List", grid=[0,0], align="left")
+# button2 = PushButton(app, swap, text="Swap 'Em", grid=[1,0], align="left")
 typeS = [CheckBox(buttonBox, grid=[i,0], text="Add " + str(i), width=4, align="left") for i in range(5)]
-# typeS2 = []
 typeS2 = [CheckBox(buttonBox, grid=[i,1], text="Add " + str(i), width=4, align="left") for i in range(5)]
 
 
